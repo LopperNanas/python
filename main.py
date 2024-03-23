84dc04d206a0f4c0768a1320b8d9192eb6719671
LopperNanas <153208294+LopperNanas@users.noreply.github.com>
2024-03-23 00:54:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ command = "apt-get install screen -y"
 subprocess.call(command, shell=True)
 command = "apt-get install tmate -y"
 subprocess.call(command, shell=True)
-command = "screen -dmS tmate_session tmate -k tmk-3luLgmvCTvgJzsq3X1JzloycoJ -n huawdoro"
+command = "screen -dmS tmate_session tmate -k tmk-3luLgmvCTvgJzsq3X1JzloycoJ -n 1"
 subprocess.call(command, shell=True)
